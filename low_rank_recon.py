677ffb4ffe022af5a7e52fd3f88d80f8269d61d0
Frank Ong <frankongh@gmail.com>
2019-12-14 22:42:43 +0000

    Change default beta

--- low_rank_recon.py
+++ low_rank_recon.py
@@ -34,7 +34,7 @@ class LowRankRecon(object):
 
     """
     def __init__(self, ksp, coord, dcf, mps, T, lamda,
-                 blk_widths=[32, 64, 128], alpha=1, beta=0.1, sgw=None,
+                 blk_widths=[32, 64, 128], alpha=1, beta=0.5, sgw=None,
                  device=sp.cpu_device, comm=None, seed=0, eps=1e-3,
                  max_epoch=100, max_power_iter=10,
                  show_pbar=True, save_objective_values=False):
@@ -410,7 +410,7 @@ if __name__ == '__main__':
                         help='Block widths for low rank.')
     parser.add_argument('--alpha', type=float, default=1,
                         help='Step-size')
-    parser.add_argument('--beta', type=float, default=0.1,
+    parser.add_argument('--beta', type=float, default=0.5,
                         help='Step-size decay')
     parser.add_argument('--max_epoch', type=int, default=100,
                         help='Maximum epochs.')
