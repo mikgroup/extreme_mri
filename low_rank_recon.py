e1f542a560247918b9fdc925ae52fcb1998530f3
Frank Ong <frankongh@gmail.com>
2019-12-17 06:48:50 +0000

    Add step size decay

--- low_rank_recon.py
+++ low_rank_recon.py
@@ -36,7 +36,7 @@ class LowRankRecon(object):
     def __init__(self, ksp, coord, dcf, mps, T, lamda,
                  blk_widths=[32, 64, 128], alpha=1, beta=0.5, sgw=None,
                  device=sp.cpu_device, comm=None, seed=0,
-                 max_epoch=50, max_power_iter=10,
+                 max_epoch=90, max_power_iter=10, K=30,
                  show_pbar=True):
         self.ksp = ksp
         self.coord = coord
@@ -45,6 +45,7 @@ class LowRankRecon(object):
         self.sgw = sgw
         self.blk_widths = blk_widths
         self.T = T
+        self.K = K
         self.lamda = lamda
         self.alpha = alpha
         self.beta = beta
@@ -185,8 +186,8 @@ class LowRankRecon(object):
                     self.img_shape)
 
     def _sgd(self):
-        for epoch in range(self.max_epoch):
-            desc = 'Epoch {}/{}'.format(epoch + 1, self.max_epoch)
+        for self.epoch in range(self.max_epoch):
+            desc = 'Epoch {}/{}'.format(self.epoch + 1, self.max_epoch)
             disable = not self.show_pbar
             total = self.T
             with tqdm(desc=desc, total=total, disable=disable, leave=True) as self.pbar:
@@ -252,7 +253,8 @@ class LowRankRecon(object):
                 raise OverflowError
 
             # Add.
-            sp.axpy(self.L[j], -self.alpha / self.G[j], g_L_j)
+            sp.axpy(self.L[j],
+                    -self.alpha / self.G[j] * self.beta**(self.epoch // self.K), g_L_j)
             sp.axpy(self.R[j][t], -self.alpha / self.G[j], g_R_jt)
 
 
@@ -270,8 +272,10 @@ if __name__ == '__main__':
                         help='Step-size')
     parser.add_argument('--beta', type=float, default=0.5,
                         help='Step-size decay')
-    parser.add_argument('--max_epoch', type=int, default=50,
+    parser.add_argument('--max_epoch', type=int, default=90,
                         help='Maximum epochs.')
+    parser.add_argument('--K', type=int, default=30,
+                        help='Epochs for step decay.')
     parser.add_argument('--device', type=int, default=-1,
                         help='Computing device.')
     parser.add_argument('--multi_gpu', action='store_true',
@@ -320,6 +324,7 @@ if __name__ == '__main__':
 
     app = LowRankRecon(ksp, coord, dcf, mps, args.T, args.lamda,
                        sgw=sgw,
+                       K=args.K,
                        blk_widths=args.blk_widths,
                        alpha=args.alpha, beta=args.beta,
                        max_epoch=args.max_epoch,
