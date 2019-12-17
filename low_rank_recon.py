bd18d323c100c0128c453f4a88549a2d49edc3b1
Frank Ong <frankongh@gmail.com>
2019-12-17 01:28:36 +0000

    Change default block width

--- low_rank_recon.py
+++ low_rank_recon.py
@@ -35,7 +35,7 @@ class LowRankRecon(object):
 
     """
     def __init__(self, ksp, coord, dcf, mps, T, lamda,
-                 blk_widths=[32, 64, 128], alpha=1, beta=0.5, sgw=None,
+                 blk_widths=[50, 100, 200], alpha=1, beta=0.5, sgw=None,
                  device=sp.cpu_device, comm=None, seed=0,
                  max_epoch=100, max_power_iter=10, variance_reduction=True,
                  show_pbar=True):
@@ -314,7 +314,7 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Low rank reconstruction.')
     parser.add_argument('--blk_widths', type=int, nargs='+',
-                        default=[32, 64, 128],
+                        default=[50, 100, 200],
                         help='Block widths for low rank.')
     parser.add_argument('--alpha', type=float, default=1,
                         help='Step-size')
