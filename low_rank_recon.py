239d2b5c97ab4a3402b8e5c42699cd5e6e4068c6
Frank Ong <frankongh@gmail.com>
2019-12-17 21:54:19 +0000

    Add back step decay

--- low_rank_recon.py
+++ low_rank_recon.py
@@ -34,8 +34,8 @@ class LowRankRecon(object):
 
     """
     def __init__(self, ksp, coord, dcf, mps, T, lamda,
-                 blk_widths=[32, 64, 128], alpha=8, beta=0.5, sgw=None,
-                 device=sp.cpu_device, comm=None, seed=0, eps=1e-2,
+                 blk_widths=[32, 64, 128], alpha=8, beta=0.1, sgw=None,
+                 device=sp.cpu_device, comm=None, seed=0, eps=1e-2, K=50,
                  max_epoch=100, max_power_iter=10,
                  show_pbar=True):
         self.ksp = ksp
@@ -54,6 +54,7 @@ class LowRankRecon(object):
         self.seed = seed
         self.max_epoch = max_epoch
         self.max_power_iter = max_power_iter
+        self.K = K
         self.show_pbar = show_pbar and (comm is None or comm.rank == 0)
 
         np.random.seed(self.seed)
@@ -252,7 +253,7 @@ class LowRankRecon(object):
                 raise OverflowError
 
             # Add.
-            sp.axpy(self.L[j], -self.alpha, g_L_j)
+            sp.axpy(self.L[j], -self.alpha * self.beta**(self.epoch // self.K), g_L_j)
             sp.axpy(self.R[j][t], -self.alpha, g_R_jt)
 
 
@@ -293,7 +294,7 @@ if __name__ == '__main__':
     parser.add_argument('T', type=int,
                         help='Number of frames.')
     parser.add_argument('lamda', type=float,
-                        help='Regularization. Recommend 1e-7 to start.')
+                        help='Regularization. Recommend 1e-8 to start.')
     parser.add_argument('img_file', type=str,
                         help='Output image file.')
 
