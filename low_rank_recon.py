2dcba3b1464bb4ec089113a11acdca3f3bbe0c96
Frank Ong <frankongh@gmail.com>
2019-12-18 21:16:06 +0000

    Change default

--- low_rank_recon.py
+++ low_rank_recon.py
@@ -34,7 +34,7 @@ class LowRankRecon(object):
 
     """
     def __init__(self, ksp, coord, dcf, mps, T, lamda,
-                 blk_widths=[32, 64, 128], alpha=5, beta=0.5, sgw=None,
+                 blk_widths=[32, 64, 128], alpha=20, beta=0.5, sgw=None,
                  device=sp.cpu_device, comm=None, seed=0, eps=1e-2,
                  max_epoch=100, max_power_iter=10,
                  show_pbar=True):
@@ -261,7 +261,7 @@ if __name__ == '__main__':
     parser.add_argument('--blk_widths', type=int, nargs='+',
                         default=[32, 64, 128],
                         help='Block widths for low rank.')
-    parser.add_argument('--alpha', type=float, default=5,
+    parser.add_argument('--alpha', type=float, default=20,
                         help='Step-size')
     parser.add_argument('--beta', type=float, default=0.5,
                         help='Step-size decay')
