7c5707e07feb7befe0a0979bf7c1357d9f0f5793
Frank Ong <frankongh@gmail.com>
2019-12-19 17:14:22 +0000

    Change parameters

--- low_rank_recon.py
+++ low_rank_recon.py
@@ -35,8 +35,8 @@ class LowRankRecon(object):
     """
     def __init__(self, ksp, coord, dcf, mps, T, lamda,
                  blk_widths=[32, 64, 128], alpha=20, beta=0.5, sgw=None,
-                 device=sp.cpu_device, comm=None, seed=0, eps=1e-2,
-                 max_epoch=100, max_power_iter=10,
+                 device=sp.cpu_device, comm=None, seed=0, eps=1e-1,
+                 max_epoch=60, max_power_iter=10,
                  show_pbar=True):
         self.ksp = ksp
         self.coord = coord
@@ -266,9 +266,9 @@ if __name__ == '__main__':
                         help='Step-size')
     parser.add_argument('--beta', type=float, default=0.5,
                         help='Step-size decay')
-    parser.add_argument('--eps', type=float, default=1e-2,
+    parser.add_argument('--eps', type=float, default=1e-1,
                         help='Initialization.')
-    parser.add_argument('--max_epoch', type=int, default=100,
+    parser.add_argument('--max_epoch', type=int, default=60,
                         help='Maximum epochs.')
     parser.add_argument('--device', type=int, default=-1,
                         help='Computing device.')
