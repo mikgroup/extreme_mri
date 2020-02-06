d1f877954e774f21c109d42ec52cefade992d030
Frank Ong <frankongh@gmail.com>
2020-02-06 19:15:52 +0000

    Add download dataset script

---
+++ download_dataset.py
@@ -0,0 +1,25 @@
+from tqdm.auto import tqdm
+import requests
+import pathlib
+
+
+def download(url, path):
+    if not path.exists():
+        path.parent.mkdir(parents=True, exist_ok=True)
+        r = requests.get(url, stream=True)
+        total_size = int(r.headers.get('content-length', 0))
+        block_size = 1024 #1 Kibibyte
+        with path.open('wb') as f:
+            with tqdm(desc='Downloading ' + path.name, total=total_size, unit='iB', unit_scale=True) as pbar:
+                for data in r.iter_content(block_size):
+                    f.write(data)
+                    pbar.update(len(data))
+
+                    
+def download_dce_dataset():
+    dcf_path = pathlib.Path('data/dce/dcf.npy')
+    coord_path = pathlib.Path('data/dce/coord.npy')
+    ksp_path = pathlib.Path('data/dce/ksp.npy')
+    download('https://zenodo.org/record/3647820/files/dcf.npy', dcf_path)
+    download('https://zenodo.org/record/3647820/files/coord.npy', coord_path)
+    download('https://zenodo.org/record/3647820/files/ksp.npy', ksp_path)
