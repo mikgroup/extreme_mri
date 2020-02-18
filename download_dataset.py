1c8d4cd03e69b548cd94cdd245e3f68e2e53a101
Frank Ong <frankong@berkeley.edu>
2020-02-18 11:49:54 -0800

    Add lung dataset

--- download_dataset.py
+++ download_dataset.py
@@ -10,12 +10,13 @@ def download(url, path):
         total_size = int(r.headers.get('content-length', 0))
         block_size = 1024 #1 Kibibyte
         with path.open('wb') as f:
-            with tqdm(desc='Downloading ' + path.name, total=total_size, unit='iB', unit_scale=True) as pbar:
+            with tqdm(desc='Downloading ' + path.name, total=total_size,
+                      unit='iB', unit_scale=True) as pbar:
                 for data in r.iter_content(block_size):
                     f.write(data)
                     pbar.update(len(data))
 
-                    
+
 def download_dce_dataset():
     dcf_path = pathlib.Path('data/dce/dcf.npy')
     coord_path = pathlib.Path('data/dce/coord.npy')
@@ -23,3 +24,12 @@ def download_dce_dataset():
     download('https://zenodo.org/record/3647820/files/dcf.npy', dcf_path)
     download('https://zenodo.org/record/3647820/files/coord.npy', coord_path)
     download('https://zenodo.org/record/3647820/files/ksp.npy', ksp_path)
+
+
+def download_lung_dataset():
+    dcf_path = pathlib.Path('data/lung/dcf.npy')
+    coord_path = pathlib.Path('data/lung/coord.npy')
+    ksp_path = pathlib.Path('data/lung/ksp.npy')
+    download('https://zenodo.org/record/3672170/files/dcf.npy', dcf_path)
+    download('https://zenodo.org/record/3672170/files/coord.npy', coord_path)
+    download('https://zenodo.org/record/3672170/files/ksp.npy', ksp_path)
